a7c7d38d65ddcf1ca38e8ae028fef562b4ec6c1c
Justin Chizer <justin.chizer@gmail.com>
2026-04-04 22:41:33 -0700

    Create the models for the data

---
+++ TransactionApi/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace TransactionAPI.Models;
+
+public class Account
+{
+    public Guid Id { get; set; }
+    public string Owner { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
