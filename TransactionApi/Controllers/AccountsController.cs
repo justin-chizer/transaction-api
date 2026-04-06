06c35f1bf526aa84153f6f2aaad76154b147c010
Justin Chizer <justin.chizer@gmail.com>
2026-04-05 22:20:21 -0700

    Accounts Controller Created

---
+++ TransactionApi/Controllers/AccountsController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TransactionAPI.Models;
+using TransactionAPI.Data;
+
+namespace TransactionAPI.Contollers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountsController : ControllerBase
+{
+    private readonly BankingDbContext _context;
+
+    public AccountsController(BankingDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
+    {
+        return await _context.Accounts.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Account>> GetAccount(Guid id)
+    {
+        var account = await _context.Accounts.FindAsync(id);
+
+        if (account == null)
+        {
+            return NotFound();
+        }
+
+        return account;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Account>> CreateAccount(Account account)
+    {
+        account.Id = Guid.NewGuid();
+        account.CreatedAt = DateTime.UtcNow;
+        account.Balance = 0;
+
+        _context.Accounts.Add(account);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAccount), new { id = account.Id }, account);
+    }
+}
