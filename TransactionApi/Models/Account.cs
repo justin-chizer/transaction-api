72bc7e9365d4ce4b1a9c2f7fdb1054084b0ed4e4
Justin Chizer <justin.chizer@gmail.com>
2026-04-14 21:57:40 -0700

    Improve the models for openapi

--- TransactionApi/Models/Account.cs
+++ TransactionApi/Models/Account.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TransactionApi.Models;
 
 public class Account
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
     public string Owner { get; set; } = string.Empty;
+    
+    [Range(0,double.MaxValue)]
     public decimal Balance { get; set; }
     public DateTime CreatedAt { get; set; }
 }
--- TransactionApi/Models/TransactionRequest.cs
+++ TransactionApi/Models/TransactionRequest.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TransactionApi.Models;
 
 public class TransactionRequest
 {
+    [Required]
+    [Range(0.01, double.MaxValue)]
     public decimal Amount { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Description { get; set; } = string.Empty;
 }
