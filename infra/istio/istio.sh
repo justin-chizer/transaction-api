23ba7589949305e0eba823661c8b89ad3778f975
justin-chizer <justin.chizer@nationsbenefits.com>
2026-04-18 23:54:46 -0700

    current helm install

---
+++ infra/istio/istio.sh
@@ -0,0 +1,5 @@
+helm repo add istio https://istio-release.storage.googleapis.com/charts
+helm repo update
+k create namespace istio-system
+helm install istio-base istio/base -n istio-system --set defaultRevision=default
+helm install istiod istio/istiod -n  istio-system --wait
