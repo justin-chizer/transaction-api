12658b0343be11185989c74ec7c5ecfaeb762119
justin-chizer <justin.chizer@nationsbenefits.com>
2026-04-19 21:15:14 -0700

    exec into the pod

--- infra/istio/istio.sh
+++ infra/istio/istio.sh
@@ -6,5 +6,9 @@ helm install istiod istio/istiod -n  istio-system --wait
 k create namespace istio-ingress
 helm install istio-ingress istio/gateway -n istio-ingress -f values.yaml
 
+k create namespace dev
 
 # sql commands to prep the Azure SQL db
+k run sqlcmd --image=mcr.microsoft.com/mssql-tools --restart=Never -n dev -- sleep 3600
+k exec -it sqlcmd -n dev -- /bin/bash
+#from the pod
