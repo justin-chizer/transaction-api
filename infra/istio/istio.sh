b56ecf4fa687e96948067ec7aeaebcfade65548f
justin-chizer <justin.chizer@nationsbenefits.com>
2026-04-19 18:02:57 -0700

    helm gateway install

--- infra/istio/istio.sh
+++ infra/istio/istio.sh
@@ -4,3 +4,4 @@ k create namespace istio-system
 helm install istio-base istio/base -n istio-system --set defaultRevision=default
 helm install istiod istio/istiod -n  istio-system --wait
 k create namespace istio-ingress
+helm install istio-ingress istio/gateway -n istio-ingress -f values.yaml
