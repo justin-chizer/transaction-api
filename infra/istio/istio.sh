d4d50d6ab6629b987794049ac3ff94902d78ba2a
Justin Chizer <justin.chizer@gmail.com>
2026-04-19 21:19:37 -0700

    new sql tes

--- infra/istio/istio.sh
+++ infra/istio/istio.sh
@@ -11,4 +11,22 @@ k create namespace dev
 # sql commands to prep the Azure SQL db
 k run sqlcmd --image=mcr.microsoft.com/mssql-tools --restart=Never -n dev -- sleep 3600
 k exec -it sqlcmd -n dev -- /bin/bash
+
 #from the pod
+sqlcmd -S <your-server>.database.windows.net \
+  -d BankingDb \
+  -G \
+  -Q "
+CREATE USER [<managed-identity-display-name>] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [<managed-identity-display-name>];
+ALTER ROLE db_datawriter ADD MEMBER [<managed-identity-display-name>];
+"
+
+curl -LO https://github.com/microsoft/go-sqlcmd/releases/download/v1.8.0/sqlcmd-linux-amd64.tar.gz
+tar -xzf sqlcmd-linux-amd64.tar.gz
+chmod +x sqlcmd
+
+./sqlcmd -S <your-server>.database.windows.net \
+  -d BankingDb \
+  --authentication-method ActiveDirectoryManagedIdentity \
+  -Q "SELECT 1"
